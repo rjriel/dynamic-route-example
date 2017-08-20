7929ccd8f1c00ef1ead8c3a947a93586f95d8b46
Rene-Jacques Riel <rie@qlik.com>
2017-08-19 22:07:42 -0400

    adding components and code needed for example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>
-      Welcome to {{title}}!!
+      Welcome
     </h1>
+    <ul>
+      <li><a routerLink="first">First</a></li>
+      <li><a routerLink="second">Second</a></li>
+    </ul>
+    <router-outlet></router-outlet>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'app';
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { FirstComponent } from './first.component'
+import { SecondComponent } from './second.component'
+
+let mainRoutes: Routes = [{
+  path: "first",
+  component: FirstComponent
+}]
+
+mainRoutes.push({
+  path: "second",
+  component: SecondComponent
+})
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
+    RouterModule.forRoot(mainRoutes),
     BrowserModule
   ],
   providers: [],
